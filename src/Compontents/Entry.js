e1ff848d704986396a4cd57e9bf761e50f896fbe
Ian <ipatrickenn7@gmail.com>
2022-08-18 15:46:48 -0600

    Added animations to title and image

---
+++ src/Compontents/Entry.js
@@ -0,0 +1,20 @@
+import title from '../Images/title.png'
+import spaceship from '../Images/spaceship.png'
+
+function Entry({ animate }) {
+    console.log("animate:", animate)
+
+    return (
+        <div id="entry_container">
+            <div id="entry_title_container">
+                <img id="entry_title_animated" src={title} alt="flight_horizons"/>
+            </div>
+            <div id="entry_visual_container">
+                <img id="entry_visual_animated" src={spaceship} alt="entry_visual"/>
+            </div>
+        </div>
+    )
+}
+
+export default Entry
+
