0f35f4812ea386c1b3d576bc6cfc69eed0da7eee
Ian <ipatrickenn7@gmail.com>
2022-08-24 14:21:59 -0600

    first commit with functioning carousel template

---
+++ src/Compontents/BootstrapCarousel.js
@@ -0,0 +1,64 @@
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Carousel } from 'react-bootstrap';
+
+function BootstrapCarousel() {
+  return (
+    // <div>
+    //   <div className="container-fluid">
+    //     <div className="row">
+    //       <div className="col-sm-12">
+    //         <h3>React Bootstrap Carousel</h3>
+    //       </div>
+    //     </div>
+    //     <div className="row">
+    //       <div className="col-12">
+    //         <Carousel>
+    //           <Carousel.Item>
+    //             <img
+    //               className="d-block w-100"
+    //               src="https://picsum.photos/500/300?img=1"
+    //               alt="First slide"
+    //             />
+    //             <Carousel.Caption>
+    //               <h3>First slide label</h3>
+    //               <p>
+    //                 Nulla vitae elit libero, a pharetra augue mollis interdum.
+    //               </p>
+    //             </Carousel.Caption>
+    //           </Carousel.Item>
+    //           <Carousel.Item>
+    //             <img
+    //               className="d-block w-100"
+    //               src="https://picsum.photos/500/300?img=2"
+    //               alt="Second slide"
+    //             />
+    //             <Carousel.Caption>
+    //               <h3>Second slide label</h3>
+    //               <p>
+    //                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    //               </p>
+    //             </Carousel.Caption>
+    //           </Carousel.Item>
+    //           <Carousel.Item>
+    //             <img
+    //               className="d-block w-100"
+    //               src="https://picsum.photos/500/300?img=3"
+    //               alt="Third slide"
+    //             />
+    //             <Carousel.Caption>
+    //               <h3>Third slide label</h3>
+    //               <p>
+    //                 Praesent commodo cursus magna, vel scelerisque nisl
+    //                 consectetur.
+    //               </p>
+    //             </Carousel.Caption>
+    //           </Carousel.Item>
+    //         </Carousel>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+  );
+}
+
+export default BootstrapCarousel
