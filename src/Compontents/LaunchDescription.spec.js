9549d322b11842ac68eac71adff27370fd252a54
Ian <ipatrickenn7@gmail.com>
2022-09-06 12:14:58 -0600

    first commit with CRA only

--- src/Compontents/LaunchDescription.spec.js
+++ src/Compontents/LaunchDescription.spec.js
@@ -1,10 +1,16 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
 import { render, screen } from "@testing-library/react"
-import { LaunchDescription } from "./LaunchDescription"
+import LaunchDescription from "./LaunchDescription"
+
+// test('use jsdom in this test file', () => {
+//     const element = document.createElement('div');
+//     expect(element).not.toBeNull();
+//   });
 
 describe("LaunchDescription", () => {
     test("renders correctly", () => {
         render(<LaunchDescription/>);
-        const nameElement = screen.getByRole('link')
-        expect(nameElement).toBeInTheDocument()
+        expect(screen.getByRole('link')).toBeInTheDocument()
     })
 })
