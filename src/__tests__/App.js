6d4efa455643260d676c1cade04a914623adba14
Ian <ipatrickenn7@gmail.com>
2022-10-20 12:16:36 -0600

    TS configured , converting files to .tsx top-down

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -4,6 +4,7 @@ import { act } from "react-test-renderer";
 import App from "../App";
 
 // Fake timers to mock setTimeout() functionality within <App /> component for entry screen
+
 // Tests that <Home /> component renders after timers expire during initial page load
 beforeAll(() => {
   jest.useFakeTimers();
